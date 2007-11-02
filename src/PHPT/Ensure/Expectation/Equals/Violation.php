f2afdcc7810bdcf1ddefeaaaa335ca0c73b29e67
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-11-02 02:23:30 +0000

    Refactor to simpler syntax
    git-svn-id: https://svn.phpt.info/Ensure/trunk@452 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/Ensure/Expectation/Equals/Violation.php
+++ src/PHPT/Ensure/Expectation/Equals/Violation.php
@@ -2,21 +2,17 @@
 
 class PHPT_Ensure_Expectation_Equals_Violation 
 {
-    private $_actual = null;
-    private $_expected = null;
-    
     public function __construct(PHPT_Ensure_Policy $policy, $expected)
     {
-        $this->_actual = $policy->value;
-        $this->_expected = $expected;
+        $this->_message = sprintf(
+            "values [%s] and [%s] are not equal",
+            var_export($policy->value, true),
+            var_export($expected, true)
+        );
     }
     
     public function __toString()
     {
-        return sprintf(
-            "values [%s] and [%s] are not equal",
-            var_export($this->_actual, true),
-            var_export($this->_expected, true)
-        );
+        return $this->_message;
     }
 }
