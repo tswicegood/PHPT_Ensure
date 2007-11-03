1f71e488d9a875fc94b549bca29d6f3f6e00ac0e
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-11-03 03:15:53 +0000

    Replace the is-a with a has-a relationship
    git-svn-id: https://svn.phpt.info/Ensure/trunk@466 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/Ensure/ExpectationAbstract/SimpleViolation.php
+++ src/PHPT/Ensure/ExpectationAbstract/SimpleViolation.php
@@ -1,13 +1,20 @@
 <?php
 
-abstract class PHPT_Ensure_ExpectationAbstract_SimpleViolation extends PHPT_Ensure_Message
+abstract class PHPT_Ensure_ExpectationAbstract_SimpleViolation
 {
+    protected $_message = '';
+    
     public function __construct(PHPT_Ensure_Policy $policy, $expected)
     {
-        $this->_message = sprintf(
+        $this->_message = new PHPT_Ensure_Message(sprintf(
             $this->_message,
             var_export($policy->value, true),
             var_export($expected, true)
-        );
+        ));
+    }
+    
+    public function __toString()
+    {
+        return (string)$this->_message;
     }
 }
